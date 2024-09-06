ba058c38d5421f8ce26b604a244afcf3d54b1090
Paul B <beckerp86@gmail.com>
2024-09-06 18:57:41 -0400

    Removed time api, removed dayjs. Added date/time logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TimeService } from '../sevices/time.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'holt-schedule';
+  public svc = inject(TimeService);
 }
